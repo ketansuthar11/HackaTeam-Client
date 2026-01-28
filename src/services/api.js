1a928faeadcb5f07db917d75f5e85adf9a15b0bf
ketan <ketansu55@gmail.com>
2026-01-29 00:31:23 +0530

    improvements

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://hackateam.vercel.app//api',
+    baseURL: 'https://hackateam.vercel.app/api',
     headers: {
         'Content-Type': 'application/json',
     },
